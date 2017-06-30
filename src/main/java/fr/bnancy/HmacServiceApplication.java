8a957a7e91ebfa5f0228e68256ff3d54e4907ea6
Bertrand Nancy <bertrand@slimpay.com>
2017-06-30 10:20:17 +0200

    Initial commit

---
+++ src/main/java/fr/bnancy/HmacServiceApplication.java
@@ -0,0 +1,12 @@
+package fr.bnancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmacServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmacServiceApplication.class, args);
+	}
+}
